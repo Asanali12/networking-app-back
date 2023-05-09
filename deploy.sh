ca1b3f7b54ed4aa73d6da5e647ca5d710ff06fb9
asanali12 <sapswung@gmail.com>
2023-05-09 04:01:06 +0300

    Workflow check 5

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+export $(grep -v '^#' ~/.env | xargs)
+pkill -f runserver
+
+sudo docker login -u $DOCKER_LOGIN -p $DOCKER_PASSWORD
+sudo docker system prune --all -f
+sudo docker-compose down
+sudo docker-compose pull
+sudo docker-compose up -d
